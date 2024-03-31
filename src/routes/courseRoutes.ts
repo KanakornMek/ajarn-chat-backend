a86a156c0fabb9e99faa8743d184e6fa2bfdb8ad
fongisthe1 <155869214+fongisthe1@users.noreply.github.com>
2024-03-31 18:28:48 +0700

    feat: updates feature/message with correct code

--- src/routes/courseRoutes.ts
+++ src/routes/courseRoutes.ts
@@ -3,7 +3,7 @@ import { getCourses, joinCourse } from '../controllers/courseController';
 import threadRoutes from './threadRoutes';
 const router = express.Router();
 
-router.use('/threads',threadRoutes);
+router.use('/:course_id/threads',threadRoutes);
 router.get('/', getCourses);
 router.post('/join', joinCourse);
 
--- src/routes/threadRoutes.ts
+++ src/routes/threadRoutes.ts
@@ -1,8 +1,10 @@
 import express from 'express';
 import { getThreads, createThread } from '../controllers/threadController';
+import messageRoutes from '../routes/messageRoutes';
 
 const router = express.Router();
 
+router.use('/:thread_id/messages', messageRoutes);
 router.get('/', getThreads);
 router.post('/', createThread);
 
