bb44bacc90ea2915b153aa69d5f6b6c6edbbba53
KanakornMek <kanakorn.meka@gmail.com>
2024-04-21 13:24:07 +0700

    fix

--- src/helpers/parseUrgencyInit.ts
+++ src/helpers/parseUrgencyInit.ts
@@ -9,6 +9,8 @@ export default function parseUrgencyInit(urgency: string): UrgencyTag| undefined
             return UrgencyTag.urgent;
         case 'regular':
             return UrgencyTag.regular;
+        case 'announcements':
+            return UrgencyTag.announcement
         default:
             return UrgencyTag.lowPriority;
     }
