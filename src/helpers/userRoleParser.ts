01f2f34320a9e289a2c590d30d059e8e261b9610
KanakornMek <kanakorn.meka@gmail.com>
2024-04-21 09:32:10 +0700

    fix: parse User Role function

--- src/helpers/userRoleParser.ts
+++ src/helpers/userRoleParser.ts
@@ -2,11 +2,11 @@ import { UserRole } from "@prisma/client";
 
 export default function parseUserRole(role: string): UserRole {
     switch (role.toUpperCase()) {
-      case 'ADMIN':
+      case 'Student':
         return UserRole.Student;
-      case 'USER':
+      case 'TA':
         return UserRole.TA;
-      case 'MODERATOR':
+      case 'Lecturer':
         return UserRole.Lecturer;
       default:
         return UserRole.Student; // or handle invalid input accordingly
